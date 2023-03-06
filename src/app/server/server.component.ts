0bfca294f8707671e2d5eb8f1b7e9f84723cfc56
NicktheGatorfin <nicholasroberts95@gmail.com>
2023-03-05 22:30:07 -0600

    Added all Udemy Angular Files to repo to work on at home and at work

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { style } from '@angular/animations';
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
+})
+
+export class ServerComponent {
+
+}
